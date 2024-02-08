3102f4447694e2e74878999017cb80c202eba572
jluengasmach <jhon.luengasmachado@mercadolibre.com.co>
2024-02-08 10:44:19 -0500

    User's age API

--- edad/src/main/java/com/api/edad/controller/ControllerEdad.java
+++ edad/src/main/java/com/api/edad/controller/ControllerEdad.java
@@ -1,6 +1,8 @@
 package com.api.edad.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Calendar;
@@ -11,7 +13,7 @@ import java.util.GregorianCalendar;
 public class ControllerEdad {
     
     @GetMapping("/{dia}/{mes}/{anio}")
-    public int getMethodName(
+    public ResponseEntity<Integer> getMethodName(
         @PathVariable int dia,
         @PathVariable int mes,
         @PathVariable int anio
@@ -19,14 +21,19 @@ public class ControllerEdad {
         Calendar fechaNacimiento = new GregorianCalendar(anio, mes - 1, dia);
         Calendar fechaHoy = new GregorianCalendar();
 
+        if (fechaHoy.compareTo(fechaNacimiento) < 0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
         int diff = fechaHoy.get(Calendar.YEAR) - fechaNacimiento.get(Calendar.YEAR);
-        if (fechaNacimiento.get(Calendar.MONTH) < fechaHoy.get(Calendar.MONTH) || 
+
+        if (fechaNacimiento.get(Calendar.MONTH) > fechaHoy.get(Calendar.MONTH) || 
             (
                 fechaNacimiento.get(Calendar.MONTH) == fechaHoy.get(Calendar.MONTH) &&
-                fechaNacimiento.get(Calendar.DATE) < fechaHoy.get(Calendar.DATE)
+                fechaNacimiento.get(Calendar.DATE) > fechaHoy.get(Calendar.DATE)
             )) {
             diff--;
         }
-        return diff;
+        return ResponseEntity.ok().body(diff);
     }
 }
